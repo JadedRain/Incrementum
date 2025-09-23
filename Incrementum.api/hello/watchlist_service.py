fb320dbd9109f30bb3298c7587e35bf4c604a75f
ChristopherTYoung <c.t.ythegamer@gmail.com>
2025-09-23 11:57:18 -0600

    Fixed add to and remove from watchlist

--- Incrementum.api/hello/watchlist_service.py
+++ Incrementum.api/hello/watchlist_service.py
@@ -1,3 +1,4 @@
+import logging
 import pandas as pd
 import os
 from .get_stock_info import fetch_stock_data
@@ -5,7 +6,7 @@ from .stocks_class import Stock
 
 class WatchlistService:
     def __init__(self):
-        self.watchlist = ["AAPL", "TSLA"]  # list of dicts: {symbol, companyName}
+        self.watchlist = ["AAPL", "TSLA"]
         base_dir = os.path.dirname(os.path.abspath(__file__))
         csv_path = os.path.join(base_dir, 'data', 'ticker_info.csv')
         self.tickers = pd.read_csv(csv_path, index_col=0)
@@ -15,9 +16,9 @@ class WatchlistService:
             return self.watchlist
         row = self.tickers[self.tickers['symbol'] == symbol]
         if not row.empty:
-            company_name = row.iloc[0]['companyName']
             if not any(item == symbol for item in self.watchlist):
                 self.watchlist.append(symbol)
+                logging.info(f"Made it to here {self.watchlist}")
         return self.watchlist
 
     def get(self):
