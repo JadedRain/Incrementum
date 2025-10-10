042386e029f6517c377570ff30ee1c404f6a60e5
ChristopherTYoung <c.t.ythegamer@gmail.com>
2025-10-10 14:49:29 -0600

    Removed the views file

--- Incrementum.api/hello/views.py
+++
@@ -1,6 +0,0 @@
-# This file previously contained Django REST Framework views
-# All views have been refactored into controller classes:
-# - StocksController (stocks_controller.py)
-# - FiltersController (filters_controller.py) 
-# - CustomCollectionController (custom_collection_controller.py)
-# - WatchlistController (watchlist_controller.py)
