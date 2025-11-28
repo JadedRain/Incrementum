5d72f9ba685921ff4b1fbde772c645eae2bb2f46
Nathan Howell <pokreman06@gmail.com>
2025-11-28 11:15:20 -0700

    hello

--- Incrementum.api/Incrementum/services/watchlist_service.py
+++ Incrementum.api/Incrementum/services/watchlist_service.py
@@ -1,12 +1,14 @@
 import logging
 from Incrementum.get_stock_info import fetch_stock_data
-from Incrementum.models import Watchlist, StockModel, WatchlistStock
+from Incrementum.models import Watchlist, StockModel, WatchlistStock, CustomCollection
 from Incrementum.models_user import Account
+from Incrementum.services.custom_collection_service import CustomCollectionService
 
 
 class WatchlistService:
     def __init__(self, fetch_stock_data_func=fetch_stock_data):
         self.fetch_stock_data_func = fetch_stock_data_func
+        self.serv = CustomCollectionService()
 
     def add(self, user_id, symbol):
         try:
@@ -14,42 +16,51 @@ class WatchlistService:
         except Account.DoesNotExist:
             logging.error(f"Account with api_key {user_id} does not exist.")
             return []
-        watchlist, _ = Watchlist.objects.get_or_create(
-            account=account,
-            defaults={"name": f"{account.name}'s Watchlist"},
-        )
         stock, _ = StockModel.objects.get_or_create(
             symbol=symbol,
             defaults={"company_name": symbol},
         )
-        WatchlistStock.objects.get_or_create(watchlist=watchlist, stock=stock)
+        watchlist = self.serv.add_stocks(
+            collection_name="Default Collection",
+            account_api_key=user_id,
+            symbols = [symbol]
+        )
+        print("adding to watchlist")
         logging.info(f"Added {symbol} to user {user_id} watchlist.")
         return self.get(user_id)
 
     def get(self, user_id):
         try:
             account = Account.objects.get(api_key=user_id)
-            watchlist = Watchlist.objects.get(account=account)
+            serv = CustomCollectionService()
+            watchlist = self.serv._get_or_create_collection_for_account(
+                collection_name="Default Collection",
+                account=account
+            )
         except (Account.DoesNotExist, Watchlist.DoesNotExist):
+            print("getting empty watchlist")
             return []
         symbols = watchlist.stocks.values_list('symbol', flat=True)
         return [self.fetch_stock_data_func(symbol).to_dict() for symbol in symbols]
 
     def remove(self, user_id, symbol):
-        try:
-            account = Account.objects.get(api_key=user_id)
-            watchlist = Watchlist.objects.get(account=account)
-            stock = StockModel.objects.get(symbol=symbol)
-        except (Account.DoesNotExist, Watchlist.DoesNotExist, StockModel.DoesNotExist):
-            return self.get(user_id)
-        WatchlistStock.objects.filter(watchlist=watchlist, stock=stock).delete()
+        account = Account.objects.get(api_key=user_id)
+        watchlist = self.serv.remove_stocks(
+            collection_name="Default Collection",
+            account_api_key=user_id,
+            symbols=[symbol]
+            
+        )
         logging.info(f"Removed {symbol} from user {user_id} watchlist.")
         return self.get(user_id)
 
     def search(self, user_id, query, max_results=10):
         try:
             account = Account.objects.get(api_key=user_id)
-            watchlist = Watchlist.objects.get(account=account)
+            watchlist = self.serv._get_or_create_collection_for_account(
+                collection_name="Default Collection",
+                account=account
+            )
         except (Account.DoesNotExist, Watchlist.DoesNotExist):
             return []
         query = query.lower()
