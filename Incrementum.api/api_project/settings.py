458abc0ab8c73e5b31bb6d5291938a29865100f7
Nathan Howell <pokreman06@gmail.com>
2025-12-01 01:30:29 -0700

    linting

--- Incrementum.api/api_project/settings.py
+++ Incrementum.api/api_project/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = ['api', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
