723748a3eeb598b1046c906f51af5bb35364853a
ChristopherTYoung <c.t.ythegamer@gmail.com>
2026-01-16 10:08:03 -0700

    changed to init.sql file to create tables if they don't already exist

--- db_init/init.sql
+++ db_init/init.sql
@@ -3,7 +3,7 @@ create schema if not exists incrementum;
 -- Set the search path to use the incrementum schema by default
 set search_path to incrementum, public;
 
-create table incrementum.account (
+create table if not exists incrementum.account (
     id int primary key generated always as identity,
     name varchar(20) not null,
     phone_number varchar(15) not null unique,
@@ -13,7 +13,7 @@ create table incrementum.account (
     keycloak_id varchar(255) unique null
 );
 
-create table incrementum.stock (
+create table if not exists incrementum.stock (
     symbol varchar(10) primary key,
     company_name varchar(100) not null,
     stock_history_updated_at timestamp not null default current_timestamp,
@@ -49,13 +49,13 @@ create table if not exists incrementum.stock_history (
     primary key (stock_symbol, day_and_time)
 );
     
-create table incrementum.screener (
+create table if not exists incrementum.screener (
     id int primary key generated always as identity,
     screener_name varchar(20) not null,
     description varchar(300)
 );
 
-create table incrementum.custom_screener (
+create table if not exists incrementum.custom_screener (
     id int primary key generated always as identity,
     account_id int not null references incrementum.account(id),
     screener_name varchar(100) not null,
@@ -63,7 +63,7 @@ create table incrementum.custom_screener (
     filters json not null
 );
 
-create table incrementum.custom_collection (
+create table if not exists incrementum.custom_collection (
     id int primary key generated always as identity,
     account_id int not null references incrementum.account(id),
     collection_name varchar(20) not null,
@@ -72,7 +72,7 @@ create table incrementum.custom_collection (
     date_created date not null
 );
 
-create table incrementum.custom_collection_stock (
+create table if not exists incrementum.custom_collection_stock (
     collection_id int not null references incrementum.custom_collection(id),
     stock_symbol varchar(10) not null references incrementum.stock(symbol)
 );
