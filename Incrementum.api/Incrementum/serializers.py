893c0f4b83cc5d34290621c99d0701d0b35a617c
ChristopherTYoung <c.t.ythegamer@gmail.com>
2026-04-06 14:03:17 -0600

    fixed linting error

--- Incrementum.api/Incrementum/serializers.py
+++ Incrementum.api/Incrementum/serializers.py
@@ -12,5 +12,6 @@ class StockSerializer(serializers.ModelSerializer):
 class UserStockPotentialSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserStockPotential
-        fields = ['id', 'account', 'stock_symbol', 'purchase_date', 'quantity', 'purchase_price']
+        fields = ['id', 'account', 'stock_symbol', 'purchase_date', 'quantity', 
+                  'purchase_price']
         read_only_fields = ['id']
